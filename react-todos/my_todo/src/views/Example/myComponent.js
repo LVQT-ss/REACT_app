58ee94f59df3aedccb37514b214e903c60b7abb2
LVQT <levietquocthinh@gmail.com>
2023-12-01 15:04:14 +0700

    state react

---
+++ react-todos/my_todo/src/views/Example/myComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+
+    //key:value
+    state = {
+        name : ' ethinhle ',
+        chanel:'hoi dan it '
+    }
+    // return block 
+    // cách để bọc 2 div thì dùng react.fragment hoặc <></>
+    // state là 1 object có key và value 
+
+    handleOnChangeName = (event) => { 
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    render() {
+        let name = 'Eric';
+
+        console.log('my name is : ', name); // Move console.log outside of the return statement
+
+        return (
+            <>
+            <div className='first'> 
+            <input value={this.state.name} type='text'
+            onChange={(event) => this.handleOnChangeName(event)}
+            />
+            this is not a div </div>
+            <div>
+                hello my component my name is {this.state.name}
+            </div>
+            <div>my ytb is sht {this.state.chanel} </div>
+            </>
+        );
+    }
+}
+
+export default MyComponent;
