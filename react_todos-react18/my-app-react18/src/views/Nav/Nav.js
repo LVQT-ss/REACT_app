acedf0d59f3e2f5246645bc94fecd84da39a99b5
LVQT <levietquocthinh@gmail.com>
2023-12-17 23:12:15 +0700

    link and fix navlink

--- react_todos-react18/my-app-react18/src/views/Nav/Nav.js
+++ react_todos-react18/my-app-react18/src/views/Nav/Nav.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './Nav.scss'
+import {  Routes, NavLink} from 'react-router-dom';
 class Nav extends React.Component{
     render(){
         return(
             <div className="topnav">
-  <a className="active" href="/">Home</a>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/todo">Todo</NavLink>
+                <NavLink to="/about">About</NavLink>
+  {/* <a className="active" href="/">Home</a>
   <a href="/todo">Todo</a>
-  <a href="/about">About</a>
+  <a href="/about">About</a> */}
 </div>
         )
 
