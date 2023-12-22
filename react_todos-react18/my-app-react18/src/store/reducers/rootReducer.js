983ef32bb6016a182473d6d3838f0ed9953fa574
LVQT <levietquocthinh@gmail.com>
2023-12-22 11:27:58 +0700

    redux and apply

---
+++ react_todos-react18/my-app-react18/src/store/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initState=  { 
+    users: [
+        {id:1,name:'eric'},
+        {id:2,name:'hehe'}
+    ]
+}
+
+const rootReducer = (state = initState, action) => { 
+
+
+    return state;
+}
+
+export default rootReducer;
